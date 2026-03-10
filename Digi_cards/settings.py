08d8d3316f6daf31d3d058220cd7e956f333987a
Mama <mamadoumbia68@gmail.com>
2026-03-10 11:15:19 +0100

    Restore DATABASES configuration in settings.py for SQLite backend

--- Digi_cards/settings.py
+++ Digi_cards/settings.py
@@ -68,12 +68,12 @@ DATABASES = {
     'default': dj_database_url.parse(config('DATABASE_URL', default='sqlite:///db.sqlite3')),
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 
